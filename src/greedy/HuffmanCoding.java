336a737a1f7784e4b373164e99bb4b96f6dade4c
rezaa <reza.amirzade1380@gmail.com>
2023-05-13 00:15:53 +0330

    "sixth commit"

---
+++ src/greedy/HuffmanCoding.java
@@ -0,0 +1,26 @@
+package greedy;
+
+
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class HuffmanCoding {
+
+    public Node huffman(List<Node> Nodes){
+        Queue<Node> queue = new PriorityQueue(Nodes);
+
+        while (queue.size() > 0){
+            Node u = queue.poll();
+            Node v = queue.poll();
+            Node z = new Node("z", 0, null);
+
+            z.setFrequency(u.getFrequency() + v.getFrequency());
+            u.setParent(z);
+            v.setParent(z);
+
+            queue.add(z);
+        }
+        return queue.peek();
+    }
+}

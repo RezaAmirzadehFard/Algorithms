713dcbba8464fecb9bfa9442f7a766906935d1c2
rezaa <reza.amirzade1380@gmail.com>
2023-09-16 14:07:04 +0330

    "add BU matrixChain"

--- src/dynamicProgarmming/matrixchain/BottomUpMatrixChain.java
+++ src/dynamicProgarmming/matrixchain/BottomUpMatrixChain.java
@@ -5,7 +5,7 @@ public class BottomUpMatrixChain {
         int n = p.length;
         int r[][] = new int[n - 1][n - 1];
 
-        for (int l = 1; l <= n - 1; l++) {        // length of sequence
+        for (int l = 1; l <= n - 1; l++) {        // length of the sequence
             for (int i = 1; i <= n - l; i++) {    // start of multiply
                 int j = i + l;                 // end of multiply
                 int min = Integer.MAX_VALUE;
@@ -21,5 +21,32 @@ public class BottomUpMatrixChain {
             }
         }
         return r[1][n - 1];
+    }
+
+
+    public static int[][] matrixChainOrder(int[] p) {
+        int n = p.length - 1;
+        int[][] m = new int[n][n];
+        int[][] s = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            m[i][i] = 0;
+        }
+
+        for (int l = 2; l <= n; l++) {
+            int min = Integer.MAX_VALUE;
+            for (int i = 1; i <= l + i - 1; i++) {
+                int j = l + i - 1;
+                for (int k = i; k <= j; k++) {
+                    int q = m[i][k] + m[k][j] + p[i - 1] * p[k] * p[j];
+                    if (q < min){
+                        min = q;
+                        s[i][j] = k;
+                    }
+                }
+                m[i][j] = min;
+            }
+        }
+        return m;
     }
 }
